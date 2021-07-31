9c9c40ad29102ee743c9001adeb01bce6538abc7
Natalha Roberto <87498345+natalharoberto@users.noreply.github.com>
2021-07-30 23:47:32 -0300

    events comms

---
+++ controls/controls.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+
+export default class Controls extends LightningElement {  handleAdd() {
+    factors = [0,2,3,4,5,6];
+    this.dispatchEvent(new CustomEvent('add'));
+  }
+  handleSubtract() {
+    this.dispatchEvent(new CustomEvent('subtract'));
+  }   handleMultiply(event) {
+    const factor = event.target.dataset.factor;
+    this.dispatchEvent(new CustomEvent('multiply', {
+      detail: factor
+    }));
+  }
+}
